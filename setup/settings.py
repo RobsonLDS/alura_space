c8275f5e7beac9dead52cc17854a5aa149067a9c
Robson Lopes da Silva <robson.silva@reitoria.unicamp.br>
2024-06-21 17:10:37 -0300

    #7 - projeto alura space

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria', #* APP da Galeria
+    'galeria.apps.GaleriaConfig', #* APP da Galeria
 ]
 
 MIDDLEWARE = [
